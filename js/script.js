c22a383e9cf89d10a073d89403e5fdf6af99ebf8
MaxPredathor <max.gilli98@gmail.com>
2023-10-09 15:57:58 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -5,15 +5,20 @@ const scontoVenti = 20/100;
 const scontoQuaranta = 40/100
 
 let km = prompt('Inserisci i kilometri che devi percorrere')
-console.log(km)
-// if(parseInt(km)){  
-// }else{
-//     prompt('Questo non è un kilometraggio valido, reinserire il valore')
-//     console.log(km)
-// }
+
+if(parseInt(km)){  
+ }else{
+     alert('Questo non è un kilometraggio valido, reinserire il valore')
+     window.location.reload()
+}
 
 let età = prompt('Insereisci la tua età');
-console.log(età);
+
+if(parseInt(età)){  
+}else{
+    alert('Questo non è un valore valido, reinserire il valore corretto')
+    window.location.reload()
+}
 
 let costo = (km * tariffa);
 console.log(costo);
@@ -28,4 +33,7 @@ if(età < 18){
     costoTotale = costo
 }
 
-console.log(costoTotale)
+document.getElementById('text').innerHTML = `
+${costoTotale.toFixed(2)}
+`
+
